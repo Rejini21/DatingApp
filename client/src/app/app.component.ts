de60ba783eca9866099b3cfb848fbf09c136e377
Susan <susan@qlinksoftware.com>
2023-05-16 17:28:51 +0300

    End of Section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { LocalizedString } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +13,22 @@ export class AppComponent implements OnInit {
   title = 'Dating App';
   users: any;
 
-  constructor(private http: HttpClient) {}
-  ngOnInit(): void {
-    this.http.get('http://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
-
+  constructor(private accountService: AccountService) {}
+  ngOnInit(): void { 
+    this.setCurrentUser();
   }
-}
+
+  
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);     
+   }
+
+   logout(){
+      this.accountService.logout();
+   }
+ }
+
+
